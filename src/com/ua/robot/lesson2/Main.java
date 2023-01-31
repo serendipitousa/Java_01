5b2336b39dee54512196a9637c2a72f56dce3120
serendipitousa <serendipitousa@gmail.com>
2023-01-31 17:10:58 +0200

    test commit

--- src/com/ua/robot/lesson2/Main.java
+++ src/com/ua/robot/lesson2/Main.java
@@ -2,6 +2,6 @@ package com.ua.robot.lesson2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World! I LOVE MYSELF!!!");
+        System.out.println("Hello World! I LOVE MYSELF!!!");
     }
 }
