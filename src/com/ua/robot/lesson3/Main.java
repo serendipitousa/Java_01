8e419cc1f88547d61b1068818cc59b2b13687e22
serendipitousa <serendipitousa@gmail.com>
2023-01-31 16:48:46 +0200

    Initial commit

---
+++ src/com/ua/robot/lesson3/Main.java
@@ -0,0 +1,14 @@
+package com.ua.robot.lesson3;
+
+public class Main {
+    public static void main(String[] args) {
+        int number1 = 6;
+        int number2 = 3;
+        System.out.println(number1 + number2);
+        System.out.println(number1 - number2);
+        System.out.println(number1 / number2);
+        System.out.println(number1 * number2);
+
+        }
+
+}
---
+++ src/com/ua/robot/lesson4/Main.java
@@ -0,0 +1,9 @@
+package com.ua.robot.lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello world";
+
+        System.out.println(str);
+    }
+}
