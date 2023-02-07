f202f48edca1214b88897b599e81e4833cfbd172
serendipitousa <serendipitousa@gmail.com>
2023-02-07 09:44:03 +0200

    homework 4

---
+++ src/com/ua/robot/lesson4/homework4/Main.java
@@ -0,0 +1,13 @@
+package com.ua.robot.lesson4.homework4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String str = "Confidence_In_The_Future";
+        System.out.println(str);
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.charAt(0));
+        System.out.println(str.charAt(str.length() - 1));
+    }
+}
